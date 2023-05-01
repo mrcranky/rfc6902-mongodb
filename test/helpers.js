c5033e278ef737b1de2d77d57075e0fd94ded7c3
Chris Chapman <chris@blackcompanystudios.co.uk>
2023-05-01 16:26:15 +0100

    Shift to using mongodb-memory-server and the real mongodb driver, as the previous mock module didn't support $pull operator

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import mongodb from 'mongo-mock';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongodb from 'mongodb';
 
 import { applyPatch } from 'rfc6902';
 
@@ -7,27 +8,31 @@ import { updatesForPatch } from '../index.js'; // module under test
 import cloneDeep from 'lodash.clonedeep';
 
 const { MongoClient } = mongodb;
-mongodb.max_delay = 0; // choose to NOT pretend wait for async operations
 
 // Connection URL
 const dbURL = 'mongodb://localhost:27017/test';
 
+let mongod;
 let client, db, collection;
 export async function openDB() {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
     expect(client, 'double call to openDB without corresponding closeDB').to.be.undefined;
-    client = await MongoClient.connect(dbURL, {});
+    client = await MongoClient.connect(uri, {});
     expect(client).to.not.be.null;
     db = client.db();
     collection = db.collection('documents');
-    return { client, db, collection };
+    return { mongod, client, db, collection };
 }
 
 export async function closeDB() {
-    await db.close();
     await client.close();
     client = undefined;
     db = undefined;
     collection = undefined;
+    await mongod.stop();
+    mongod = undefined;
 }
 
 export async function clearCollection() {
@@ -37,13 +42,13 @@ export async function clearCollection() {
 export async function applyPatchUpdates(query, updates) {
     for (const update of updates) {
         const updateResult = await collection.updateOne(query, update);
-        expect(updateResult.result.ok).to.equal(1);
+        expect(updateResult.acknowledged).to.be.true;
     }
 }
 
 export async function checkUpdatesProduceCorrectResult(message, originalDocument, patch) {
-    const insertResult = await collection.insertOne(originalDocument);
-    expect(insertResult.result.ok).to.equal(1);
+    const insertResult = await collection.insertOne({ ...originalDocument }); // Deliberately clone to avoid the parameter being modified by the insert
+    expect(insertResult.acknowledged).to.be.true;
     const query = { _id: insertResult.insertedId };
 
     const updates = updatesForPatch(patch, originalDocument);
--- test/mongo.test.js
+++ test/mongo.test.js
@@ -1,24 +1,15 @@
 import { expect } from 'chai';
-import { openDB, closeDB } from './helpers.js';
+import { openDB, closeDB, clearCollection } from './helpers.js';
 
 // Test of the test rig (does not exercise module code)
 describe('Test that mongodb updates can be performed', async function() {
-    let client;
-    let db;
     let collection;
     before('Set up mongo server', async function() {
         const results = await openDB();
-        client = results.client;
-        db = results.db;
         collection = results.collection;
     });
-    after('Close client', async function() {
-        await closeDB(client, db);
-    });
-
-    afterEach('Clear collection', async function() {
-        await collection.deleteMany({});
-    });
+    after('Close client', closeDB);
+    afterEach('Clear collection', clearCollection);
 
     it('can insert a simple document and retrieve it after', async function() {
         const document1 = {
@@ -26,7 +17,7 @@ describe('Test that mongodb updates can be performed', async function() {
         };
         const result1 = await collection.insertOne(document1);
         const document2 = await collection.findOne(document1);
-        expect(document2._id).to.equal(result1.insertedId);
+        expect(document2._id.equals(result1.insertedId)).to.be.true;
         expect(document2.foo).to.equal('bar');
     });
 
@@ -40,11 +31,11 @@ describe('Test that mongodb updates can be performed', async function() {
             },
         };
         const result1 = await collection.insertOne(document1);
-        expect(result1.result.ok).to.equal(1);
+        expect(result1.acknowledged).to.be.true;
         const result2 = await collection.updateOne({ _id: result1.insertedId }, update);
-        expect(result2.result.ok).to.equal(1)
+        expect(result2.acknowledged).to.be.true;
         const document3 = await collection.findOne({ _id: result1.insertedId });
-        expect(document3._id).to.equal(result1.insertedId);
+        expect(document3._id.equals(result1.insertedId)).to.be.true;
         expect(document3.foo).to.equal('baz');
     });
 
@@ -53,9 +44,9 @@ describe('Test that mongodb updates can be performed', async function() {
             foo: 'bar',
         };
         const result1 = await collection.insertOne(document1);
-        expect(result1.result.ok).to.equal(1);
+        expect(result1.acknowledged).to.be.true;
         const result2 = await collection.deleteOne({ _id: result1.insertedId });
-        expect(result2.result.ok).to.equal(1)
+        expect(result2.acknowledged).to.be.true;
         const document3 = await collection.findOne({ _id: result1.insertedId });
         expect(document3).to.be.null;
     });
